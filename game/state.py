ab574c413a641c0202b02c749e8e9a9ad8179c25
Krish Singh <singh.krish@yahoo.com>
2026-03-13 16:12:46 -0700

    feat: broke everything

--- game/state.py
+++ game/state.py
@@ -15,7 +15,7 @@ class GameState:
     MYSORE_CARDS = ["Iron Rockets", "Sepoy Mutiny", "French Alliance", "Monsoon", "Cavalry Raid", "Sea Trade"]
     BRITISH_CARDS = ["Wall Breach", "Highlanders", "Royal Navy", "Divide and Rule", "Force March", "Princely States"]
 
-    TERRITORY_TO_IDX = {name: i for i, name in enumerate(TERRITORIES)}
+    NODE_TO_IDX = {name: i for i, name in enumerate(TERRITORIES)}
     WHO_TO_MOVE_TO_IDX = {name: i for i, name in enumerate(WHO_TO_MOVE)}
     MYSORE_CARDS_TO_IDX = {name: i for i, name in enumerate(MYSORE_CARDS)}
     BRITISH_CARDS_TO_IDX = {name: i for i, name in enumerate(BRITISH_CARDS)}
@@ -45,20 +45,20 @@ class GameState:
     def default_setup(self):
         self.reset_cards()
 
-        self.set_territory_vector_fresh_army("Bombay")
-        self.set_territory_vector_fresh_army("Hyderabad")
-        self.set_territory_vector_fresh_army("Madras")
-        self.set_territory_vector_fresh_army("Travancore")
+        self.set_node_fresh_army("Bombay")
+        self.set_node_fresh_army("Hyderabad")
+        self.set_node_fresh_army("Madras")
+        self.set_node_fresh_army("Travancore")
 
-        self.set_territory_vector_fort("Darwar")
-        self.set_territory_vector_fort("Bednore")
-        self.set_territory_vector_fort("Mangalore")
-        self.set_territory_vector_fort("Bangalore")
-        self.set_territory_vector_fort("Srirangapatna")
-        self.set_territory_vector_fort("Erode")
-        self.set_territory_vector_fort("Coimbatore")
-        self.set_territory_vector_fort("Mahé")
-        self.set_territory_vector_fort("Palgautcherry")
+        self.set_node_fort("Darwar")
+        self.set_node_fort("Bednore")
+        self.set_node_fort("Mangalore")
+        self.set_node_fort("Bangalore")
+        self.set_node_fort("Srirangapatna")
+        self.set_node_fort("Erode")
+        self.set_node_fort("Coimbatore")
+        self.set_node_fort("Mahé")
+        self.set_node_fort("Palgautcherry")
 
         self.set_turn(1)
         self.set_who_to_move_by_name("British Move")
@@ -68,51 +68,22 @@ class GameState:
         new_state = GameState()
         new_state.vector = np.copy(self.vector)
         return new_state
-    
-    def reset_cards(self):
-        self.vector[self.IDX_BRITISH_CARDS] = True
-        self.vector[self.IDX_MYSORE_CARDS] = True
 
-    def use_card_mysore_by_name(self, mysore_card):
-        self.use_card_mysore_by_value(self.MYSORE_CARDS_TO_IDX[mysore_card])
+    def set_node_fresh_army(self, node):
+        self.set_node_empty(node)
+        self.vector[self.IDX_TERRITORIES_OFFSET + 3 * self.NODE_TO_IDX[node]] = True
 
-    def use_card_mysore_by_value(self, mysore_card_value):
-        self.vector[self.IDX_MYSORE_CARDS_OFFSET + mysore_card_value] = False
+    def set_node_tired_army(self, node):
+        self.set_node_empty(node)
+        self.vector[self.IDX_TERRITORIES_OFFSET + 3 * self.NODE_TO_IDX[node] + 1] = True
 
-    def use_card_british_by_name(self, british_card):
-        self.use_card_british_by_value(self.BRITISH_CARDS_TO_IDX[british_card])
+    def set_node_fort(self, node):
+        self.set_node_empty(node)
+        self.vector[self.IDX_TERRITORIES_OFFSET + 3 * self.NODE_TO_IDX[node] + 2] = True
 
-    def use_card_british_by_value(self, british_card_value):
-        self.vector[self.IDX_BRITISH_CARDS_OFFSET + british_card_value] = False
-
-    def set_territory_vector_fresh_army(self, territory):
-        self.set_territory_vector_empty(territory)
-        self.vector[self.IDX_TERRITORIES_OFFSET + 3 * self.TERRITORY_TO_IDX[territory]] = True
-
-    def set_territory_vector_tired_army(self, territory):
-        self.set_territory_vector_empty(territory)
-        self.vector[self.IDX_TERRITORIES_OFFSET + 3 * self.TERRITORY_TO_IDX[territory] + 1] = True
-
-    def set_territory_vector_fort(self, territory):
-        self.set_territory_vector_empty(territory)
-        self.vector[self.IDX_TERRITORIES_OFFSET + 3 * self.TERRITORY_TO_IDX[territory] + 2] = True
-
-    def set_territory_vector_empty(self, territory):
-        start_idx = self.IDX_TERRITORIES_OFFSET + 3 * self.TERRITORY_TO_IDX[territory]
+    def set_node_empty(self, node):
+        start_idx = self.IDX_TERRITORIES_OFFSET + 3 * self.NODE_TO_IDX[node]
         self.vector[start_idx : start_idx + 3] = False
-    
-    def next_turn(self):
-        self.turn += 1
-    
-    def set_who_to_move_by_name(self, who_to_move):
-        self.set_who_to_move_by_value(self.WHO_TO_MOVE_TO_IDX[who_to_move])
-
-    def set_who_to_move_by_value(self, who_to_move_idx):
-        self.vector[self.IDX_WHO_TO_MOVE] = False
-        self.vector[self.IDX_WHO_TO_MOVE_OFFSET + (who_to_move_idx % 3)] = True
-    
-    def next_to_move(self):
-        self.set_who_to_move_by_value(self.to_move + 1)
 
     def clear_combat(self):
         self.vector[self.IDX_COMBATANTS] = False
@@ -120,7 +91,7 @@ class GameState:
     
     def queue_combat_by_name(self, attacker_name, defender_name):
         self.vector[self.IDX_COMBAT_STRENGTH_OFFSET] = True
-        self.queue_combat_by_value(self.TERRITORY_TO_IDX[attacker_name], self.TERRITORY_TO_IDX[defender_name])
+        self.queue_combat_by_value(self.NODE_TO_IDX[attacker_name], self.NODE_TO_IDX[defender_name])
 
     def queue_combat_by_value(self, attacker_idx, defender_idx):
         self.vector[self.IDX_COMBATANTS_OFFSET + attacker_idx] = True
@@ -129,6 +100,11 @@ class GameState:
     def set_combat_strength(self, card_idx):
         self.vector[self.IDX_COMBAT_STRENGTH_OFFSET] = False
         self.vector[self.IDX_COMBAT_STRENGTH_OFFSET + self.CARD_VALUE[card_idx]] = True
+
+    def turn_refresh(self):
+        self.turn += 1
+        self.mysore_cards = True
+        self.british_cards = True
 
     @property
     def fresh_armies(self):
@@ -192,7 +168,7 @@ class GameState:
 def main():
     default = GameState()
     default.default_setup()
-    default.set_territory_vector_tired_army("Travancore")
+    default.set_node_tired_army("Travancore")
     default.queue_combat_by_name("Travancore", "Palgautcherry")
     default.set_combat_strength(0)
     default.use_card_mysore_by_name("Iron Rockets")
