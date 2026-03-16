71efdc6516d06381f09a6c0fad5e05febe539c9a
Min Lim <min.v.lim07@berkeley.edu>
2026-03-16 02:06:16 -0700

    Fixed: Copy button

--- game/frontend/script.js
+++ game/frontend/script.js
@@ -402,10 +402,19 @@ function saveState() {
     copyBtn.className = 'action-btn safe';
     copyBtn.innerText = '📋 Copy to Clipboard';
     copyBtn.onclick = () => {
-        navigator.clipboard.writeText(binStr).then(() => {
-            copyBtn.innerText = '✅ Copied!';
-            setTimeout(() => copyBtn.innerText = '📋 Copy to Clipboard', 2000);
-        });
+      var textarea = document.createElement("textarea");
+      textarea.value = binStr;
+      textarea.setAttribute("readonly", "");
+      textarea.style.position = "absolute";
+      textarea.style.left = "-9999px";
+      document.body.appendChild(textarea);
+      textarea.select();
+      textarea.setSelectionRange(0, 99999);
+      document.execCommand("copy");
+      document.body.removeChild(textarea);
+
+      copyBtn.innerText = '✅ Copied!';
+      setTimeout(() => copyBtn.innerText = '📋 Copy to Clipboard', 2000);
     };
 
     // 6. Add Close Button
